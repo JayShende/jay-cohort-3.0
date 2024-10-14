b3ca380af74a3370f2db9a24e171faec8dd533c0
JayShende <shendejay379@gmail.com>
2024-10-14 12:10:38 +0530

    Completed the project with Input validation using ZOD

--- Week 7/server.js
+++ Week 7/server.js
@@ -1,103 +1,143 @@
-const express=require("express");
+const express = require("express");
 // const UserModel=require("./db");
 // const TodosModel=require("./db");
 
+const { z } = require("zod");
+const { auth } = require("./auth");
 
-const {auth}=require("./auth");
-
-const jwt=require("jsonwebtoken");
-const JWT_SECRET="JayShende007@";
-const bcrypt=require("bcrypt");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "JayShende007@";
+const bcrypt = require("bcrypt");
 
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://Jayshende:S12Ce8MKll5AtCPl@cluster0.i3qcn.mongodb.net/todo-app-database")
+mongoose.connect(
+  "mongodb+srv://Jayshende:S12Ce8MKll5AtCPl@cluster0.i3qcn.mongodb.net/todo-app-database"
+);
 //  or we can also do at once as
-const {UserModel,TodosModel}=require("./db");
+const { UserModel, TodosModel } = require("./db");
 
-
-const app=express();
+const app = express();
 app.use(express.json());
-
 
 // Writting an Auth Middleware which checks weather the token recived as valid or not
 
+app.post("/signup", async function (req, res) {
+  // We Should Check First Weather the credentials have been used or not and then work accordingly
+  const email = req.body.email;
+  const password = req.body.password;
+  const name = req.body.name;
 
+  // Input Validation Using Zod
 
+  const schema = z.object({
+    name: z.string().max(100),
+    email: z.string().min(5).max(20).email(),
+    password: z
+      .string()
+      .min(8)
+      .max(20)
+      .refine(
+        function (password) {
+          return /[a-z]/.test(password);
+        },
+        {
+          message: "Password must contain at least one lowercase letter",
+        }
+      )
+      .refine(
+        function (password) {
+          return /[A-Z]/.test(password);
+        },
+        {
+          message: "Password must contain at least one Uppercase letter",
+        }
+      )
+      .refine(
+        function (password) {
+          return /[\W_]/.test(password);
+        },
+        {
+          message: "Password must contain at least one special character",
+        }
+      )
+      .refine(function(password){
+        return /[0-9]/.test(password);
+      },{
+        message:"The Password must contain at least one number "
+      })
+  });
 
-app.post("/signup",async function(req,res){
-    
-    // We Should Check First Weather the credentials have been used or not and then work accordingly
-    const email=req.body.email;
-    const password=req.body.password;
-    const name=req.body.name;
-    const hashedpwd=await bcrypt.hash(password,5);
+  const result = schema.safeParse(req.body);
+
+  if (result.success) {
+    const hashedpwd = await bcrypt.hash(password, 5);
     await UserModel.create({
-        name:name,
-        email:email,
-        password:hashedpwd
+      name: name,
+      email: email,
+      password: hashedpwd,
     });
 
     res.json({
-        message:"You Are Now Signed in"
-    })
-     
-});
-
-
-app.post("/signin",async function(req,res){
-
-    // This endpoint Check if the Credentails are avaliable in the db and then issue an token accordingly
-    const email=req.body.email;
-    const password=req.body.password;
-
-    const response=await UserModel.findOne({
-        email:email,
+      message: "You Are Now Signed in",
     });
-    console.log(response);
-
-    const ans=await bcrypt.compare(password,response.password);
-
-    if(ans)
-    {
-        const token=jwt.sign({
-            id:response._id.toString() // its an object so we have to convert it to string to continue
-        },JWT_SECRET);
-        res.json({
-            token:token
-        });
-    }
-    else{
-        res.status(403).send({
-            message:"Incorrect Credentials"
-        });
-    }
+  } else {
+    res.send(result.error);
+  }
 });
 
+app.post("/signin", async function (req, res) {
+  // This endpoint Check if the Credentails are avaliable in the db and then issue an token accordingly
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const response = await UserModel.findOne({
+    email: email,
+  });
+  console.log(response);
+
+  const ans = await bcrypt.compare(password, response.password);
+
+  if (ans) {
+    const token = jwt.sign(
+      {
+        id: response._id.toString(), // its an object so we have to convert it to string to continue
+      },
+      JWT_SECRET
+    );
+    res.json({
+      token: token,
+    });
+  } else {
+    res.status(403).send({
+      message: "Incorrect Credentials",
+    });
+  }
+});
 
 // Both the Below Todos are Authenticated ie Can Only Be Hittted when the User is Loggedin
 
-app.post("/todos",auth,async function(req,res){
-    // res.send(req.userID);
-    const title =req.body.title;
-    await TodosModel.create({
-        UserId:req.userID,
-        title:title,
-        done:false
-    });
+app.post("/todos", auth, async function (req, res) {
+  // res.send(req.userID);
+  const title = req.body.title;
+  await TodosModel.create({
+    UserId: req.userID,
+    title: title,
+    done: false,
+  });
 
-    res.json({
-        message:"To Added"
-    });
+  res.json({
+    message: "To Added",
+  });
 });
 
 // The Below Api Endpoint is used to Retrive all The Todos Specific to a User
 
-app.get("/todos",auth,async function(req,res){
-    const response=await TodosModel.find({
-        UserId:req.userID
-    });
+app.get("/todos", auth, async function (req, res) {
+  const response = await TodosModel.find({
+    UserId: req.userID,
+  });
 
-    res.send(response);
+  res.send(response);
 });
 
 app.listen(3000);
